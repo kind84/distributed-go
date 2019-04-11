c9d5b01ac87b8a3e17adb8ad9ba1100d7801acfa
Paolo Grisoli <paolo_g84@icloud.com>
2019-04-11 11:27:24 +0200

    Chapter 2

---
+++ chapter2/recovering/recover.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func simpleFunc(index int, wg *sync.WaitGroup) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from", r)
+		}
+	}()
+	defer wg.Done()
+	fmt.Println("Attempting x/(x-10) where x = ", index, " answer is : ", index/(index-10))
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(40)
+
+	for i := 0; i < 40; i++ {
+		go func(j int) {
+			simpleFunc(j, &wg)
+		}(i)
+	}
+
+	wg.Wait()
+}
